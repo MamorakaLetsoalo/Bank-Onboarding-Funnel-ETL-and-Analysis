b88fe0cc37228f17d3a5f7878f372cb16b30a90c
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-03-28 13:36:34 +0200

    create dim customer table

---
+++ SQLScripts/DimensionTables.sql
@@ -0,0 +1,18 @@
+USE Banking_Warehouse;
+
+-- dim customers
+CREATE TABLE dbo.dim_customer (
+    customer_id INT IDENTITY(1,1) PRIMARY KEY,   -- surrogate key
+    customer_identifier VARCHAR(50),             -- business key
+
+    age_group VARCHAR(20),
+    gender VARCHAR(10),
+    income_band VARCHAR(20),
+    employment_status VARCHAR(50),
+    is_unbanked_flag BIT,
+    kyc_level VARCHAR(20),
+
+    effective_date DATETIME DEFAULT GETDATE(),
+    expiry_date DATETIME,
+    is_current BIT DEFAULT 1
+);
