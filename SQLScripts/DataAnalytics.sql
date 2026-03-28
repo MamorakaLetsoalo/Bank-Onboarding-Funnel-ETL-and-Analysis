d302a08f1c9196e7dc5e66eb3612c742eba229cb
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-03-28 18:41:49 +0200

    create channel performance view

--- SQLScripts/DataAnalytics.sql
+++ SQLScripts/DataAnalytics.sql
@@ -59,3 +59,25 @@ SELECT
 FROM dbo.fact_onboarding_funnel f
 JOIN dbo.dim_step s ON f.step_id = s.step_id
 GROUP BY s.step_name;
+
+GO
+
+--channel performance view
+CREATE VIEW dbo.vw_onboarding_channel AS
+SELECT
+    c.channel_name,
+
+    COUNT(DISTINCT f.customer_id) AS total_users,
+
+    SUM(CASE WHEN f.completion_flag = 1 THEN 1 ELSE 0 END) AS completed_users,
+    SUM(CASE WHEN f.drop_off_flag = 1 THEN 1 ELSE 0 END) AS dropped_users,
+
+    CAST(
+        SUM(CASE WHEN f.completion_flag = 1 THEN 1 ELSE 0 END) * 1.0 
+        / COUNT(DISTINCT f.customer_id)
+    AS DECIMAL(10,4)) AS conversion_rate
+
+FROM dbo.fact_onboarding_funnel f
+JOIN dbo.dim_channel c 
+    ON f.channel_id = c.channel_id
+GROUP BY c.channel_name;
