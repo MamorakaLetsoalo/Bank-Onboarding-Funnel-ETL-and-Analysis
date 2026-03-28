98a890b1ce4a28238cfbc1cb09740beeea23570c
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-03-28 11:47:47 +0200

    creating databases

---
+++ SQLScripts/DatabaseSchemaTables.sql
@@ -0,0 +1,8 @@
+-- Raw layer (stores everything)
+CREATE DATABASE Banking_Raw;
+
+-- Staging layer(stores clean and standerdised data)
+CREATE DATABASE Banking_Staging;
+
+-- Warehouse layer(used for analysis)
+CREATE DATABASE Banking_Warehouse;
